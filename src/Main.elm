f3521cec63d70101cf89dcc1a325613af4494e9e
Luis Couto <hello@luiscouto.pt>
2019-03-03 17:41:37 +0000

    WIP

--- src/Main.elm
+++ src/Main.elm
@@ -1,24 +1,27 @@
-module Main exposing (..)
+module Main exposing (main)
 
-import Html
-import Html.Styled exposing (..)
-import Html.Styled.Attributes exposing (attribute, value, type_, placeholder)
-import Html.Styled.Events exposing (on, onInput, onSubmit, onClick)
-import Json.Decode as Decode
-import Styled exposing (..)
+import Browser
+import Element exposing (..)
+import Element.Border as Border exposing (..)
+import Element.Input as Input exposing (..)
+import Html exposing (Html)
+import Prng.Uuid as Uuid
+import Random.Pcg.Extended exposing (Seed, initialSeed, step)
 
 
-main : Program Never Model Msg
 main =
-    Html.beginnerProgram
-        { model = model
+    Browser.sandbox
+        { init = init
+        , view = view
         , update = update
-        , view = view >> toUnstyled
         }
 
 
-
--- MODEL
+type alias Task =
+    { text : String
+    , author : String
+    , status : TaskStatus
+    }
 
 
 type TaskStatus
@@ -27,157 +30,97 @@ type TaskStatus
     | Done
 
 
-type alias Task =
-    { id : Int
-    , title : String
-    , status : TaskStatus
-    }
-
-
 type alias Model =
-    { currentId : Int
-    , currentTask : String
-    , tasks : List Task
-    , dragging : Maybe Task
+    { tasks : List Task
+    , createTaskInputText : String
     }
 
 
-model : Model
-model =
-    { currentId = 0
-    , currentTask = ""
-    , tasks = []
-    , dragging = Nothing
-    }
-
-
-
--- UPDATE
+filterTasks : TaskStatus -> List Task -> List Task
+filterTasks status tasks =
+    List.filter (\task -> task.status == status) tasks
 
 
 type Msg
-    = Drag Task
-    | Drop TaskStatus
-    | Create
-    | InputChange String
-    | Delete Task
+    = CreateTask
+    | CreateTaskInputChanged String
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Drag task ->
-            { model | dragging = Just task }
+        CreateTaskInputChanged text ->
+            { model | createTaskInputText = text }
 
-        Drop taskStatus ->
+        CreateTask ->
             { model
-                | dragging = Nothing
-                , tasks = switchBoard taskStatus model.tasks model.dragging
+                | tasks =
+                    Task model.createTaskInputText
+                        "Tyler Durden"
+                        Backlog
+                        :: model.tasks
+                , createTaskInputText = ""
             }
 
-        InputChange title ->
-            { model | currentTask = title }
 
-        Create ->
-            { model
-                | currentId = model.currentId + 1
-                , tasks =
-                    if String.length model.currentTask > 0 then
-                        Task model.currentId model.currentTask Backlog :: model.tasks
-                    else
-                        model.tasks
-                , currentTask = ""
+viewCreateTask : Model -> Element Msg
+viewCreateTask model =
+    Element.row [ Element.width fill, spacing 10 ]
+        [ Input.text []
+            { onChange = CreateTaskInputChanged
+            , text = model.createTaskInputText
+            , placeholder = Just (Input.placeholder [] (Element.text "Create a task"))
+            , label = Input.labelHidden "label"
             }
-
-        Delete task ->
-            { model | tasks = List.filter (\t -> t /= task) model.tasks }
-
-
-switchBoard : TaskStatus -> List Task -> Maybe Task -> List Task
-switchBoard newStatus tasks desiredTask =
-    List.map
-        (\task ->
-            case desiredTask of
-                Just dtask ->
-                    if task.id == dtask.id then
-                        { dtask | status = newStatus }
-                    else
-                        task
-
-                _ ->
-                    task
-        )
-        tasks
+        , Input.button []
+            { onPress = Just CreateTask
+            , label = Element.text "submit"
+            }
+        ]
 
 
+viewTask : Task -> Element Msg
+viewTask task =
+    task.text
+        |> Element.text
+        |> List.singleton
+        |> Element.paragraph []
+        |> Element.el
+            [ Element.width fill
+            , spacing 10
+            ]
 
--- VIEW
+
+viewColumn : List Task -> Element Msg
+viewColumn tasks =
+    tasks
+        |> List.map viewTask
+        |> column
+            [ Element.width fill
+            , height fill
+            , Border.color (rgb 0 0 0)
+            , Border.widthEach
+                { top = 0
+                , right = 1
+                , bottom = 0
+                , left = 0
+                }
+            ]
 
 
 view : Model -> Html Msg
 view model =
-    let
-        filterTasks status tasks =
-            List.filter (\task -> task.status == status) tasks
-
-        backlogTasks =
-            filterTasks Backlog model.tasks
-
-        inProgressTasks =
-            filterTasks InProgress model.tasks
-
-        doneTasks =
-            filterTasks Done model.tasks
-    in
-        div []
-            [ globalStyle
-            , formBoard [ onSubmit Create ]
-                [ inputTask
-                    [ onInput InputChange
-                    , value model.currentTask
-                    , placeholder "What needs to be done?"
-                    ]
-                    []
-                , input [ type_ "submit", value "Create Task" ] []
-                ]
-            , board []
-                [ columnView Backlog (List.map taskView backlogTasks)
-                , columnView InProgress (List.map taskView inProgressTasks)
-                , columnView Done (List.map taskView doneTasks)
+    Element.layout [] <|
+        column [ Element.width fill, height fill, spacing 10 ]
+            [ viewCreateTask model
+            , row [ Element.width fill, height fill ]
+                [ viewColumn (filterTasks Backlog model.tasks)
+                , viewColumn (filterTasks InProgress model.tasks)
+                , viewColumn (filterTasks Done model.tasks)
                 ]
             ]
 
 
-taskView : Task -> Html Msg
-taskView taskModel =
-    task
-        [ attribute "data-task" (toString taskModel.id)
-        , attribute "draggable" "true"
-        , onDragStart <| Drag taskModel
-        ]
-        [ closeButton [ onClick (Delete taskModel) ] [ text "Delete task" ]
-        , p [] [ text taskModel.title ]
-        ]
-
-
-columnView : TaskStatus -> List (Html Msg) -> Html Msg
-columnView status tasks =
-    column
-        [ attribute "ondragover" "return false", onDrop <| Drop status ]
-        [ tasks
-            |> List.length
-            |> toString
-            |> text
-        , text " Task(s)"
-        , taskList [] tasks
-        ]
-
-
-onDragStart : Msg -> Attribute Msg
-onDragStart msg =
-    on "dragstart" (Decode.succeed msg)
-
-
-onDrop : Msg -> Attribute Msg
-onDrop msg =
-    on "drop" (Decode.succeed msg)
+init : Model
+init =
+    { tasks = [], createTaskInputText = "" }
